a86214b74f0238f941070b4bc05e364cd139dd08
AlexStasiuk <sov853972@gmail.com>
2021-04-25 21:43:50 +0300

    add restrictions in controller layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
   def new 
-
   end
   def create #handles post request on login path
     user = User.find_by(username: params[:session][:username])
@@ -19,5 +19,13 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
+  end
+  
+  private
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 end
